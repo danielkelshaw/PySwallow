1b0a8fd1dc047482b852110e01f3bbceb7b817fa
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-01-06 19:16:34 +0000

    Updated sopso.py
    sopso.py now inherits the base classes and overrides the relevant methods to ensure smooth operation.
    
    Resolves: #14

--- pyswallow/opt/sopso.py
+++ pyswallow/opt/sopso.py
@@ -1,48 +1,31 @@
+from ..base.base_swarm import BaseSwarm
+from ..base.base_swallow import BaseSwallow
+
 import numpy as np
 import copy
 
 
-class Swallow:
+class Swallow(BaseSwallow):
 
     def __init__(self, lb, ub):
-        self.lb = lb
-        self.ub = ub
-
-        self.position = np.random.uniform(lb, ub, size=self.lb.shape[0])
-        self.velocity = np.random.uniform(lb, ub, size=self.lb.shape[0])
-        self.fitness = None
-
-        self.pbest_position = self.position
-        self.pbest_fitness = float('inf')
+        super().__init__(lb, ub)
 
     def move(self):
         self.position += self.velocity
 
 
-class Swarm:
+class Swarm(BaseSwarm):
 
     def __init__(self, obj_function, n_swallows, n_iterations,
                  lb, ub, constraints=None, w=0.7, c1=2.0, c2=2.0):
 
+        super().__init__(n_swallows, lb, ub, w, c1, c2)
+
         self.obj_function = obj_function
         self.constraints = constraints
 
-        self.n_swallows = n_swallows
-        self.n_iterations = n_iterations
-
-        self.lb = np.array(lb)
-        self.ub = np.array(ub)
-        assert self.lb.shape[0] == self.ub.shape[0]
-
-        self.w = w
-        self.c1 = c1
-        self.c2 = c2
-
         self.iteration = 0
-        self.population = []
-
-        self.gbest_position = np.random.uniform(lb, ub, size=self.lb.shape[0])
-        self.gbest_fitness = float('inf')
+        self.n_iterations = n_iterations
 
     # Reset Methods
     def reset_environment(self):
@@ -61,12 +44,12 @@ class Swarm:
                            for _ in range(self.n_swallows)]
 
     # Update Methods
-    def eval_fitness(self, swallow):
+    def evaluate_fitness(self, swallow):
         swallow.fitness = self.obj_function(swallow.position)
 
-    def swarm_eval_fitness(self):
+    def swarm_evaluate_fitness(self):
         for swallow in self.population:
-            self.eval_fitness(swallow)
+            self.evaluate_fitness(swallow)
 
     def update_velocity(self, swallow):
 
@@ -95,28 +78,23 @@ class Swarm:
         if self.constraints is not None:
             if self.constraints(swallow.position):
                 if swallow.fitness < swallow.pbest_fitness:
-                    swallow.pbest_fitness = swallow.fitness
-                    swallow.pbest_position = swallow.position
+                    swallow.pbest_fitness = copy.deepcopy(swallow.fitness)
+                    swallow.pbest_position = copy.deepcopy(swallow.position)
         else:
             if swallow.fitness < swallow.pbest_fitness:
-                swallow.pbest_fitness = swallow.fitness
-                swallow.pbest_position = swallow.position
+                swallow.pbest_fitness = copy.deepcopy(swallow.fitness)
+                swallow.pbest_position = copy.deepcopy(swallow.position)
 
-    def swarm_pbest_update(self):
-        for swallow in self.population:
-            self.pbest_update(swallow)
-
-    def gbest_update(self):
-        for swallow in self.population:
-            if self.constraints is not None:
-                if self.constraints(swallow.position):
-                    if swallow.fitness < self.gbest_fitness:
-                        self.gbest_fitness = copy.copy(swallow.fitness)
-                        self.gbest_position = copy.copy(swallow.position)
-            else:
+    def gbest_update(self, swallow):
+        if self.constraints is not None:
+            if self.constraints(swallow.position):
                 if swallow.fitness < self.gbest_fitness:
-                    self.gbest_fitness = copy.copy(swallow.fitness)
-                    self.gbest_position = copy.copy(swallow.position)
+                    self.gbest_fitness = copy.deepcopy(swallow.fitness)
+                    self.gbest_position = copy.deepcopy(swallow.position)
+        else:
+            if swallow.fitness < self.gbest_fitness:
+                self.gbest_fitness = copy.deepcopy(swallow.fitness)
+                self.gbest_position = copy.deepcopy(swallow.position)
 
     # Optimise
     def termination_check(self):
@@ -134,9 +112,8 @@ class Swarm:
 
             print('Iteration {0}: {1}'.format(self.iteration, self.gbest_fitness))
 
-            self.swarm_eval_fitness()
-            self.swarm_pbest_update()
-            self.gbest_update()
+            self.swarm_evaluate_fitness()
+            self.swarm_update_best()
 
             self.swarm_update_velocity()
             self.swarm_move()
