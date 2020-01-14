ae16837768fe075d87c93ac9615e6d20f694dc20
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-01-14 17:49:48 +0000

    Implement BaseHandler
    This will act as the basis for the boundary/velocity handlers.

---
+++ pyswallow/handlers/base_handler.py
@@ -0,0 +1,18 @@
+import inspect
+import numpy as np
+
+
+class BaseHandler:
+
+    def _get_all_strategies(self):
+        return {
+            k: v
+            for k, v in inspect.getmembers(self, predicate=inspect.isroutine)
+            if not k.startswith(['_', '__'])
+        }
+
+    @staticmethod
+    def _out_of_bounds(vector, lb, ub):
+        ltb = np.nonzero(vector < lb)[0]
+        gtb = np.nonzero(vector > ub)[0]
+        return ltb, gtb
