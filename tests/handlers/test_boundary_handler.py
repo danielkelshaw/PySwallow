2895f32c2cb32d1d36dfffaae4714af63a62ffef
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-03-30 23:35:57 +0100

    Tests for all handlers
    Unit tests are now in place for all relevant pieces of code.
    
    Resolves: #37

---
+++ tests/handlers/test_boundary_handler.py
@@ -0,0 +1,70 @@
+import pytest
+import numpy as np
+
+import pyswallow as ps
+from pyswallow.handlers.boundary_handler import (
+    StandardBH, NearestBH, ReflectiveBH, RandomBH
+)
+
+
+class TestBoundaryHandler:
+
+    @pytest.fixture
+    def standard_bh(self):
+        return StandardBH()
+
+    @pytest.fixture
+    def bounds(self):
+        lb = np.array([0, 0])
+        ub = np.array([10, 10])
+        return lb, ub
+
+    @pytest.mark.parametrize('pos', [[-5, -5], [5, 5], [15, 15]])
+    def test_standard(self, standard_bh, pos):
+        pos = np.array(pos, dtype=np.float32)
+        ret_pos = standard_bh(pos)
+        assert np.array_equal(pos, ret_pos)
+
+    # TODO >> Write better tests for these boundary handlers.
+    @pytest.mark.parametrize('pos', [[-5, -5], [5, 5], [15, 15]])
+    def test_nearest(self, bounds, pos):
+        lb, ub = bounds
+        arr_pos = np.array(pos, dtype=np.float32)
+        bh = NearestBH(lb, ub)
+        ret_pos = bh(arr_pos)
+
+        assert len(pos) == len(ret_pos)
+        assert np.logical_and(ret_pos >= lb, ret_pos <= ub).all()
+
+        if pos == [-5, -5]:
+            assert np.array_equal(ret_pos, lb)
+        elif pos == [5, 5]:
+            assert np.array_equal(ret_pos, arr_pos)
+        elif pos == [15, 15]:
+            assert np.array_equal(ret_pos, ub)
+
+    @pytest.mark.parametrize('pos', [[-5, -5], [5, 5], [15, 15]])
+    def test_reflective(self, bounds, pos):
+        lb, ub = bounds
+        arr_pos = np.array(pos, dtype=np.float32)
+        bh = ReflectiveBH(lb, ub)
+        ret_pos = bh(arr_pos)
+
+        assert len(pos) == len(ret_pos)
+        assert np.logical_and(ret_pos >= lb, ret_pos <= ub).all()
+        if pos == [-5, -5]:
+            assert np.array_equal(ret_pos, np.array([5, 5]))
+        elif pos == [5, 5]:
+            assert np.array_equal(ret_pos, arr_pos)
+        elif pos == [15, 15]:
+            assert np.array_equal(ret_pos, np.array([5, 5]))
+
+    @pytest.mark.parametrize('pos', [[-5, -5], [5, 5], [15, 15]])
+    def test_random(self, bounds, pos):
+        lb, ub = bounds
+        arr_pos = np.array(pos, dtype=np.float32)
+        bh = RandomBH(lb, ub)
+        ret_pos = bh(arr_pos)
+
+        assert len(pos) == len(ret_pos)
+        assert np.logical_and(ret_pos >= lb, ret_pos <= ub).all()
---
+++ tests/handlers/test_velocity_handler.py
@@ -0,0 +1,67 @@
+import pytest
+import numpy as np
+
+import pyswallow as ps
+from pyswallow.handlers.velocity_handler import (
+    StandardVH, ClampedVH, InvertVH, ZeroVH
+)
+
+
+class TestVelocityHandler:
+
+    @pytest.fixture
+    def bounds(self):
+        lb = np.array([0, 0])
+        ub = np.array([10, 10])
+        return lb, ub
+
+    @pytest.mark.parametrize('vel', [[-5, -5], [5, 5], [15, 15]])
+    def test_standard_vh(self, vel):
+        vel = np.array(vel, dtype=np.float32)
+        vh = StandardVH()
+        ret_vel = vh(vel)
+
+        assert np.array_equal(ret_vel, vel)
+
+    @pytest.mark.parametrize('vel', [[-5, -5], [5, 5], [15, 15]])
+    def test_clamped_vh(self, bounds, vel):
+        lb, ub = bounds
+        arr_vel = np.array(vel, dtype=np.float32)
+        vh = ClampedVH(lb, ub)
+        ret_vel = vh(arr_vel)
+
+        assert np.logical_and(ret_vel >= lb, ret_vel <= ub).all()
+        if vel == [-5, -5]:
+            assert np.array_equal(ret_vel, lb)
+        elif vel == [5, 5]:
+            assert np.array_equal(ret_vel, arr_vel)
+        elif vel == [15, 15]:
+            assert np.array_equal(ret_vel, ub)
+
+    @pytest.mark.parametrize('vel', [[-5, -5], [5, 5], [15, 15]])
+    def test_invert_vh(self, bounds, vel):
+        lb, ub = bounds
+        arr_vel = np.array(vel, dtype=np.float32)
+        vh = InvertVH(lb, ub)
+        ret_vel = vh(arr_vel, 0.5)
+
+        if vel == [-5, -5]:
+            assert np.array_equal(ret_vel, np.array([2.5, 2.5]))
+        elif vel == [5, 5]:
+            assert np.array_equal(ret_vel, arr_vel)
+        elif vel == [15, 15]:
+            assert np.array_equal(ret_vel, np.array([-7.5, -7.5]))
+
+    @pytest.mark.parametrize('vel', [[-5, -5], [5, 5], [15, 15]])
+    def test_zero_vh(self, bounds, vel):
+        lb, ub = bounds
+        arr_vel = np.array(vel, dtype=np.float32)
+        vh = ZeroVH(lb, ub)
+        ret_vel = vh(arr_vel)
+
+        if vel == [-5, -5]:
+            assert np.array_equal(ret_vel, np.zeros(2))
+        elif vel == [5, 5]:
+            assert np.array_equal(ret_vel, arr_vel)
+        elif vel == [15, 15]:
+            assert np.array_equal(ret_vel, np.zeros(2))
